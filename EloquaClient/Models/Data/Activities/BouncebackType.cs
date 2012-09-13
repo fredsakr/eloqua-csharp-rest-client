afc48d8792e881a42a6c7ecd50eba1a1cf68958e
Adam Ward <award@mktpoint.com>
2012-09-13 17:29:51 +0100

    Added Method and associated classes for returning a list of bounceback activity

---
+++ EloquaClient/Models/Data/Activities/BouncebackType.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Eloqua.Api.Rest.ClientLibrary.Models.Data.Activities
+{
+    public enum BouncebackType
+    {
+        hardBounce = 1,
+        softBounce = 2
+    }
+}
