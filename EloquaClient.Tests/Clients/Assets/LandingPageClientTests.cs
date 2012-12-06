235ec88a7d92dcf78503827c12d93a6f326aeec6
Fred Sakr <fred.sakr@eloqua.com>
2012-12-06 11:07:15 -0500

    removing unused test

--- EloquaClient.Tests/Clients/Assets/LandingPageClientTests.cs
+++ EloquaClient.Tests/Clients/Assets/LandingPageClientTests.cs
@@ -19,16 +19,5 @@ namespace Eloqua.Api.Rest.ClientLibrary.Tests.Clients.Assets
             var landingPages = _client.Assets.StructuredLandingPage.Get("fsTest*", 1, 100);
             Assert.Greater(landingPages.elements.Count, 0);
         }
-
-        [Test]
-        public void GetandPutTest()
-        {
-            var lp = _client.Assets.StructuredLandingPage.Get(2500);
-            lp.htmlContent.cssHeader = "<style type=\"text/css\">test ab</style>";
-            lp.micrositeId= 6;
-            lp.name = "fsTest sample 1";
-            lp.htmlContent.id = -500001;
-            var result = _client.Assets.StructuredLandingPage.Put(lp);
-        }
     }
 }
