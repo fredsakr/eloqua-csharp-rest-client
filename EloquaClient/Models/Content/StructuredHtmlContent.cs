924069c4d725a1c86a9622f81b256bf0845bb299
Fred Sakr <fred.sakr@eloqua.com>
2012-12-07 08:18:58 -0500

    removing unused properties from StructuredHtmlContent

--- EloquaClient/Models/Content/StructuredHtmlContent.cs
+++ EloquaClient/Models/Content/StructuredHtmlContent.cs
@@ -3,14 +3,11 @@
     public class StructuredHtmlContent
     {
         public string cssHeader { get; set; }
-        public string customHeader { get; set; }
         public string docType { get; set; }
         public string htmlBody { get; set; }
         public int? id { get; set; }
         public string javascriptHeader { get; set; }
-//        public string metaTags { get; set; }
         public string root { get; set; }
-        public string systemHeader { get; set; }
         public string type
         {
             get { return "StructuredHtmlContent"; }
